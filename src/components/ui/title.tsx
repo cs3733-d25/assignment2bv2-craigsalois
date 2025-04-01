c5a2aad9c9696ba6aaf2cbcb1ea341666b56d053
craigsalois <cmsalois04@gmail.com>
2025-03-31 21:44:11 -0400

    added files and installed shadcn and created title component

---
+++ src/components/ui/title.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title: React.FC = () => {
+    return (
+        <h1 className="text-3xl font-bold text-center mt-8">
+            CS3733-D25 Team:20 Craig and Kaito
+        </h1>
+    );
+};
+
+export default Title;
