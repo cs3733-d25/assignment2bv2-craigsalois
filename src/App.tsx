45be9bc2658fcc489b5fc8859438d771c1e16406
craigsalois <cmsalois04@gmail.com>
2025-04-02 19:16:53 -0400

    added files and installed shadcn and created title component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Title from "@/components/ui/title"
 import Intro from "@/components/ui/intro"
+import {Hobby1} from "@/components/hobby1.tsx";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <div>
           <Title />
           <Intro name1 = "Craig" name2 = "Kaito" />
+          <Hobby1/>
       </div>
 
   )
