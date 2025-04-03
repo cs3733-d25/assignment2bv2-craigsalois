3d2062d37337862bedc484cf298231d37cb0c757
craigsalois <cmsalois04@gmail.com>
2025-04-02 21:29:24 -0400

    added files and installed shadcn and created title component

--- src/components/ui/intro.tsx
+++ src/components/ui/intro.tsx
@@ -10,7 +10,7 @@ const Intro: React.FC<IntroProps> = ({ name1, name2 }) => {
         <div className="p-6 bg-white shadow-lg rounded-lg max-w-lg mx-auto text-center">
             <p className="text-lg">
                 Hi! We are {name1} and {name2}. We are part of CS3733-D25 Team 20 and excited to work on this project together!<br/>
-                {name1} is from Massachusetts and loves running.<br/> {name2} is from California and ______
+                {name1} is from Massachusetts and loves running.<br/> {name2} is from California and loves gaming, fashion, and climbing
             </p>
         </div>
     );
--- src/components/ui/title.tsx
+++ src/components/ui/title.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Title: React.FC = () => {
     return (
         <h1 className="text-3xl font-bold text-center mt-8">
-            CS3733-D25 Team 20: Running & ________
+            CS3733-D25 Team 20: Running & Games/Fashion/Climbing
         </h1>
     );
 };
